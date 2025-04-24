278b56e92695d552158536dfe4645c9c076cab16
Prokop <prokop.schield@protonmail.com>
2025-04-24 17:44:43 +0200

    refactor: move distance into utils

--- src/scatter_net/mod.rs
+++ src/scatter_net/mod.rs
@@ -1,13 +1,13 @@
 mod constants;
-mod distance;
 mod error_code;
 mod net;
 mod peer;
 mod peer_group;
+mod utils;
 
 pub use constants::*;
-pub use distance::distance;
 pub use error_code::ErrorCode;
 pub use net::{NetConfig, NetState, ScatterNet};
 pub use peer::{Peer, PeerState};
 pub use peer_group::{PeerGroup, PeerGroupConfig};
+pub use utils::*;
