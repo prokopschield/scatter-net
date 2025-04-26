7352f2643ea06673db66538cfd083991874afc24
Prokop <prokop.schield@protonmail.com>
2025-04-27 00:34:39 +0200

    feat: Peer::listen_dg

--- src/scatter_net/peer/methods/listen_dg.rs
+++ src/scatter_net/peer/methods/listen_dg.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use anyhow::Result;
 
-use crate::Peer;
+use crate::{spawn_and_forget, Packet, Peer};
 
 impl Peer {
     pub async fn listen_dg(peer: Arc<Self>) -> Result<()> {
@@ -10,8 +10,16 @@ impl Peer {
 
         loop {
             let dg = connection.read_datagram().await?;
+            let packet = Packet::from_bytes(dg)?;
+            let peer_clone = peer.clone();
 
-            let _todo = dg;
+            spawn_and_forget(async move {
+                if let Some(response) = packet.process(peer_clone.clone()).await? {
+                    peer_clone.send_datagram(response.to_bytes()?)?;
+                }
+
+                Ok(())
+            });
         }
     }
 }
