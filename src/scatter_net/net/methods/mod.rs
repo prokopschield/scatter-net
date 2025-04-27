5f47521ab026d696b2fd98c43e1f543cf6082101
Prokop <prokop.schield@protonmail.com>
2025-04-27 09:03:43 +0200

    wip: ScatterNet::put_blob

--- src/scatter_net/net/methods/mod.rs
+++ src/scatter_net/net/methods/mod.rs
@@ -5,4 +5,5 @@ mod handle_incomming_connection;
 mod init;
 mod init_peer;
 mod init_peer_groups;
+mod put_blob;
 mod terminate;
---
+++ src/scatter_net/net/methods/put_blob.rs
@@ -0,0 +1,16 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use ps_hkey::Hkey;
+
+use crate::ScatterNet;
+
+impl ScatterNet {
+    pub async fn put_blob(net: Arc<ScatterNet>, blob: &[u8]) -> Result<Hkey> {
+        let hkey = net.lake.put_blob(blob)?;
+
+        // TODO: upload blob to peers
+
+        Ok(hkey)
+    }
+}
