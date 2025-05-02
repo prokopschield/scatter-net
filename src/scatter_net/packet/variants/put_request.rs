9a86dac2f5c0ee1ab4bc9475f2534dcdc0c8717b
Prokop <prokop.schield@protonmail.com>
2025-05-02 10:41:08 +0200

    refactor: PutRequest should use Bytes instead of Vec<u8>

--- src/scatter_net/packet/variants/put_request.rs
+++ src/scatter_net/packet/variants/put_request.rs
@@ -1,9 +1,10 @@
+use bytes::Bytes;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PutRequest {
     /// Data to be stored, either a raw blob, or an encrypted `DataChunk`
-    data: Vec<u8>,
+    data: Bytes,
 
     /// Is the data an encrypted `DataChunk`?
     encrypted: bool,
