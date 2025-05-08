182c53ad4b3142a969dfb1837f677eac180867e0
Prokop <prokop.schield@protonmail.com>
2025-05-08 22:44:23 +0200

    chore: lint

--- src/scatter_net/interaction/methods/init.rs
+++ src/scatter_net/interaction/methods/init.rs
@@ -7,15 +7,13 @@ use crate::{Interaction, Peer};
 
 impl Interaction {
     pub fn init(peer: Arc<Peer>, recv_stream: RecvStream, send_stream: Option<SendStream>) -> Self {
-        let interaction = Self {
+        Self {
             buffer: Arc::default(),
             peer,
             recv_stream: Arc::new(Mutex::new(recv_stream)),
             send_stream: send_stream.map(Mutex::new).map(Arc::new),
             packets: Arc::default(),
             closed: Arc::default(),
-        };
-
-        interaction
+        }
     }
 }
