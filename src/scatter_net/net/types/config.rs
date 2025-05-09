391bb60cc648cb1c6d85235a63c746936c56da26
Prokop <prokop.schield@protonmail.com>
2025-05-09 09:34:09 +0200

    feat: NetConfig::populate_peer_groups

--- src/scatter_net/net/types/config.rs
+++ src/scatter_net/net/types/config.rs
@@ -23,6 +23,7 @@ pub struct NetConfig {
 impl NetConfig {
     pub fn populate(&mut self) -> Result<()> {
         self.populate_lake_config()?;
+        self.populate_peer_groups();
 
         Ok(())
     }
@@ -85,5 +86,22 @@ impl NetConfig {
         }
 
         Ok(())
+    }
+
+    pub fn populate_peer_groups(&mut self) {
+        for rtt in [50, 150, u64::MAX] {
+            if !self
+                .peer_groups
+                .iter()
+                .any(|peer_group| peer_group.open && peer_group.rtt_cap_ms >= rtt)
+            {
+                self.peer_groups.push(PeerGroupConfig {
+                    name: format!("LT_{rtt}ms_RTT"),
+                    open: true,
+                    rtt_cap_ms: rtt,
+                    ..Default::default()
+                });
+            }
+        }
     }
 }
