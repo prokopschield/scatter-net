38049f241de4bb3b879a0f94a2588626f7fdcb3d
Prokop <prokop.schield@protonmail.com>
2025-05-12 01:33:15 +0200

    feat: impl Display for PeerGroup

---
+++ src/scatter_net/peer_group/implementations/display.rs
@@ -0,0 +1,9 @@
+use std::fmt::Display;
+
+use crate::PeerGroup;
+
+impl Display for PeerGroup {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("PeerGroup({})", self.config.name))
+    }
+}
