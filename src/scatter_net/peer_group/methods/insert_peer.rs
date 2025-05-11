e938888151206cdc34b4d6cb250d522d6a557b67
Prokop <prokop.schield@protonmail.com>
2025-05-12 01:20:01 +0200

    feat: PeerGroup::insert_peer

---
+++ src/scatter_net/peer_group/methods/insert_peer.rs
@@ -0,0 +1,10 @@
+use std::sync::Arc;
+
+use crate::{Peer, PeerGroup};
+
+impl PeerGroup {
+    /// Inserts a [`Peer`] into this [`PeerGroup`]
+    pub fn insert_peer(&self, peer: Arc<Peer>) {
+        self.peers.write().insert(peer.node_id(), peer);
+    }
+}
--- src/scatter_net/peer_group/methods/mod.rs
+++ src/scatter_net/peer_group/methods/mod.rs
@@ -2,4 +2,5 @@ mod get_peer_by_hash;
 mod get_rtt_cap_ms;
 mod has_peer;
 mod init;
+mod insert_peer;
 mod is_open;
