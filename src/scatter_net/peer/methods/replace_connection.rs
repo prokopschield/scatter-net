daac50cd4cb70f58b182afa179692e0e2913177d
Prokop <prokop.schield@protonmail.com>
2025-05-12 10:09:02 +0200

    feat: assert node_ids equal in Peer::replace_connection

--- src/scatter_net/peer/methods/replace_connection.rs
+++ src/scatter_net/peer/methods/replace_connection.rs
@@ -1,16 +1,42 @@
-use anyhow::Result;
-use iroh::endpoint::Connection;
+use iroh::{endpoint::Connection, NodeId};
 
 use crate::Peer;
 
 impl Peer {
-    pub fn replace_connection(&self, connection: Connection) -> Result<()> {
-        *self.connection.write() = connection;
+    pub fn replace_connection(
+        &self,
+        connection: Connection,
+    ) -> Result<(), PeerReplaceConnectionError> {
+        let conn_node_id = connection
+            .remote_node_id()
+            .map_err(|err| PeerReplaceConnectionError::ReadingNodeIdFailed(err))?;
 
-        // TODO assert node_ids equal
+        let own_node_id = self.node_id();
+
+        if conn_node_id != own_node_id {
+            eprintln!("Tried to replace connection for {own_node_id} with {conn_node_id}!");
+
+            return Err(PeerReplaceConnectionError::NodeIdMismatch {
+                own_node_id,
+                conn_node_id,
+            });
+        }
+
+        *self.connection.write() = connection;
 
         eprintln!("Replaced connection for {}", self.node_id());
 
         Ok(())
     }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum PeerReplaceConnectionError {
+    #[error("Tried to replace connection to {own_node_id} with connection to {conn_node_id}.")]
+    NodeIdMismatch {
+        conn_node_id: NodeId,
+        own_node_id: NodeId,
+    },
+    #[error(transparent)]
+    ReadingNodeIdFailed(anyhow::Error),
 }
