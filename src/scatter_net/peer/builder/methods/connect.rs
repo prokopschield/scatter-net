235af3ea48c46dff5eeeef9a1c53ce0ec361b95b
Prokop <prokop.schield@protonmail.com>
2025-08-11 22:46:16 +0200

    feat: PeerBuilder::connect

---
+++ src/scatter_net/peer/builder/methods/connect.rs
@@ -0,0 +1,18 @@
+use iroh::{endpoint::ConnectError, NodeAddr};
+
+use crate::{PeerBuilder, ALPN};
+
+impl PeerBuilder {
+    /// Initializes a connection to `node_addr`, consuming the builder.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`ConnectError`] if the connection fails.
+    pub async fn connect(mut self, node_addr: impl Into<NodeAddr>) -> Result<Self, ConnectError> {
+        let connection = self.net.endpoint.connect(node_addr, ALPN).await?;
+
+        self.connection = Some(connection);
+
+        Ok(self)
+    }
+}
--- src/scatter_net/peer/builder/methods/mod.rs
+++ src/scatter_net/peer/builder/methods/mod.rs
@@ -1,3 +1,4 @@
+mod connect;
 mod finalize;
 mod new;
 mod with_direct_addresses;
