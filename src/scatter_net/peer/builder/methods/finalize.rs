989c59884f2126503bc3e4f9d8cba49a7585a6ee
Prokop <prokop.schield@protonmail.com>
2025-08-12 12:52:26 +0200

    refactor: PeerBuilder::finalize should call Peer::listen

--- src/scatter_net/peer/builder/methods/finalize.rs
+++ src/scatter_net/peer/builder/methods/finalize.rs
@@ -30,6 +30,8 @@ impl PeerBuilder {
             },
         );
 
+        peer.clone().listen();
+
         if let Some(peer_group) = peer_group {
             peer_group.insert_peer(peer.clone());
         } else {
