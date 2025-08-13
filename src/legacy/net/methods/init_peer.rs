0e698731d330a2178a99a7e591c32afb964ca9ef
Prokop <prokop.schield@protonmail.com>
2025-08-13 17:17:53 +0200

    refactor: remove redundant resource access

--- src/legacy/net/methods/init_peer.rs
+++ src/legacy/net/methods/init_peer.rs
@@ -6,9 +6,8 @@ use crate::{spawn_and_forget, Peer, PeerState, ScatterNet};
 impl ScatterNet {
     pub fn init_peer(&self, connection: Connection, state: Option<PeerState>) -> Result<Peer> {
         let node_id = connection.remote_node_id()?;
-        let mut guard = self.write();
 
-        if let Some(peer) = guard.peers.get(&node_id) {
+        if let Some(peer) = self.read().peers.get(&node_id) {
             peer.replace_connection(connection);
 
             return Ok(peer.clone());
@@ -17,10 +16,6 @@ impl ScatterNet {
         let peer = Peer::builder(self.clone(), node_id)
             .with_option_state(state)
             .finalize(connection);
-
-        guard.peers.insert(node_id, peer.clone());
-
-        drop(guard);
 
         spawn_and_forget({
             let peer = peer.clone();
